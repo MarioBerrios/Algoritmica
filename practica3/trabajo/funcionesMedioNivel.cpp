4ae4623993df288bd8b93ab01cca53fd21cee651
Mario Berrios Carmona <i12bercm@uco.es>
2022-10-25 16:23:14 +0200

    better interface

--- practica3/trabajo/funcionesMedioNivel.cpp
+++ practica3/trabajo/funcionesMedioNivel.cpp
@@ -29,10 +29,12 @@ void metodoISE(){
     serie.erroresSegmentacion(ise, errorMax, posicion);
 
     std::cout << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
     std::cout << "Número de puntos dominantes: " << serie.contarPuntosDominantes() << std::endl;
     std::cout << "ISE = " << ise << std::endl;
     std::cout << "Error máximo = " << errorMax << std::endl;
     std::cout << "Punto de error máximo: " << posicion << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
     std::cout << std::endl;
 
     char guardarFichero[50];
@@ -66,14 +68,13 @@ void metodoEMax(){
     int posicion;
     serie.erroresSegmentacion(ise, errorMax, posicion);
 
-    //std::cout << std::endl;
-    //serie.mostrarPuntosDominantes();
-
     std::cout << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
     std::cout << "Número de puntos dominantes: " << serie.contarPuntosDominantes() << std::endl;
     std::cout << "ISE = " << ise << std::endl;
     std::cout << "Error máximo = " << errorMax << std::endl;
     std::cout << "Punto de error máximo: " << posicion << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
     std::cout << std::endl;
 
     char guardarFichero[50];
